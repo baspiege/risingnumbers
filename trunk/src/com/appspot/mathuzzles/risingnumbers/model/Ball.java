4a70fcb3be3d21689660fead9866f56caf0acc29
spiegel121@gmail.com <spiegel121@gmail.com@169917c8-4e65-2226-cd43-e2cffd4eb461>
2011-01-18 04:51:55 +0000

    Initial check in.

---
+++ trunk/src/com/appspot/mathuzzles/risingnumbers/model/Ball.java
@@ -0,0 +1,13 @@
+package com.appspot.mathuzzles.risingnumbers.model;
+
+import java.io.Serializable;
+
+public class Ball implements Serializable{
+
+	public static final long serialVersionUID=1L;
+	
+	public int x;
+	public int y;
+	public int number;
+
+}
